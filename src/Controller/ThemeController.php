cd40ef0c53ea2f30d6551a14ec90b8e8c5363b2e
SebF33 <78741537+SebF33@users.noreply.github.com>
2021-11-23 12:16:13 +0100

    comment form ok

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -9,6 +9,7 @@ use App\Repository\CommentRepository;
 use App\Repository\ThemeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,13 +39,22 @@ class ThemeController extends AbstractController
     /**
      * @Route("/theme/{slug}", name="theme")
      */
-    public function show(Request $request, Theme $theme, CommentRepository $commentRepository, ThemeRepository $themeRepository)
+    public function show(Request $request, Theme $theme, CommentRepository $commentRepository, ThemeRepository $themeRepository, string $photoDir)
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setTheme($theme);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
+                }
+                $comment->setPhotoFilename($filename);
+            }
 
             $this->entityManager->persist($comment);
             $this->entityManager->flush();
